f72992be395b0fbd36ece1f2326ec196d5271673
Rodolfo Lima <rlima@nvidia.com>
2022-10-23 09:58:30 +0200

    refactor(test): add comparison operators to test::Param
    Sometimes compiler needs them.
    
    CVCUDA-162

--- tests/common/ValueTests.hpp
+++ tests/common/ValueTests.hpp
@@ -47,23 +47,23 @@ class Param
 
 public:
     template<class U = void *>
-    requires(sizeof(U) * 0 + sizeof...(DEFAULT) == 1) Param()
+    requires(sizeof(U) * 0 + sizeof...(DEFAULT) == 1) constexpr Param()
         : m_value(DEFAULT...)
     {
     }
 
     template<class U = void *>
-    requires(std::is_default_constructible_v<T> && sizeof(U) * 0 + sizeof...(DEFAULT) == 0) Param()
+    requires(std::is_default_constructible_v<T> && sizeof(U) * 0 + sizeof...(DEFAULT) == 0) constexpr Param()
         : m_value(T{})
     {
     }
 
-    Param(T value)
+    constexpr Param(T value)
         : m_value(value)
     {
     }
 
-    operator T() const
+    constexpr operator T() const
     {
         return m_value;
     }
@@ -75,6 +75,21 @@ public:
         out << std::noboolalpha;
         return out;
     };
+
+    constexpr bool operator==(const Param &that) const
+    {
+        return m_value == that.m_value;
+    }
+
+    constexpr bool operator!=(const Param &that) const
+    {
+        return !(*this == that);
+    }
+
+    constexpr bool operator<(const Param &that) const
+    {
+        return m_value < that.m_value;
+    }
 
 private:
     T m_value;
