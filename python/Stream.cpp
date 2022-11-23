cb42e7b175d9b6947c69a5ed1f0aa6bc1dccf614
Rodolfo Lima <rlima@nvidia.com>
2022-11-23 16:40:25 +0100

    fix(python): make sure stream stack is empty when script ends
    Closes CVCUDA-322

--- python/Stream.cpp
+++ python/Stream.cpp
@@ -354,6 +354,20 @@ void Stream::Export(py::module &m)
                             stream->sync();
                         }
                         globalStream->sync();
+
+                        // There should only be 1 stream in the stack, namely the
+                        // global stream.
+                        auto s = StreamStack::Instance().top();
+                        if (s != globalStream)
+                        {
+                            std::cerr << "Stream stack leak detected" << std::endl;
+                        }
+
+                        // Make sure stream stack is empty
+                        while (auto s = StreamStack::Instance().top())
+                        {
+                            StreamStack::Instance().pop();
+                        }
                     });
 }
 
