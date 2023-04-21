92c9cccde77104b86cbdb18efac12802f1bfa2e8
Dhaval Suthar <dsuthar@nvidia.com>
2023-04-21 15:35:19 -0700

    refactor: major refactoring of the segmentation sample
    allows easy plug and play of various backends for various stages
    
    Closes CVCUDA-540

--- samples/scripts/run_samples.sh
+++ samples/scripts/run_samples.sh
@@ -35,20 +35,21 @@ python3 ./classification/python/inference.py -i ./assets/images/tabby_tiger_cat.
 # Run classification sample for image directory as input with batch size 2
 python3 ./classification/python/inference.py -i ./assets/images/ -l ./models/imagenet-classes.txt -b 2
 # Run the segmentation sample with default settings, without any command-line args.
-python3 ./segmentation/python/inference.py
-# Run it on a single image with high batch size for the background class writing to a specific directory
-python3 segmentation/python/inference.py -i assets/images/tabby_tiger_cat.jpg -o /tmp -b 5 -c __background__
-# Run it on a folder worth of images
-python3 segmentation/python/inference.py -i assets/images/ -o /tmp -b 5 -c __background__
+python3 ./segmentation/python/main.py
+# Run the segmentation sample with default settings for PyTorch backend.
+python3 ./segmentation/python/main.py -bk pytorch
+# Run it on a single image with high batch size for the background class writing to a specific directory with pytorch backend
+python3 ./segmentation/python/main.py -i assets/images/tabby_tiger_cat.jpg -o /tmp -b 5 -c __background__ -bk pytorch
+# Run it on a folder worth of images with the default tensorrt backend
+python3 ./segmentation/python/main.py -i assets/images/ -o /tmp -b 4 -c __background__
+# Run it on a folder worth of images with PyTorch
+python3 ./segmentation/python/main.py -i assets/images/ -o /tmp -b 5 -c __background__ -bk pytorch
 # Run on a single image with custom resized input given to the sample for the dog class
-python3 segmentation/python/inference.py -i assets/images/Weimaraner.jpg -o /tmp -b 1 -c dog -th 224 -tw 224
-# Run the segmentation sample with default settings for TensorRT backend.
-python3 ./segmentation/python/inference.py -bk tensorrt
-# Run it on a folder worth of images with TensorRT
-python3 segmentation/python/inference.py -i assets/images/ -o /tmp -b 5 -c __background__ -bk tensorrt
+python3 ./segmentation/python/main.py -i assets/images/Weimaraner.jpg -o /tmp -b 1 -c dog -th 512 -tw 512
 # Run it on a video for class background.
-python segmentation/python/inference.py -i assets/videos/pexels-ilimdar-avgezer-7081456.mp4 -b 5 -c __background__
-
+python ./segmentation/python/main.py -i assets/videos/pexels-ilimdar-avgezer-7081456.mp4 -b 4 -c __background__
+# Run it on a video for class background with pytorch backend.
+python ./segmentation/python/main.py -i assets/videos/pexels-ilimdar-avgezer-7081456.mp4 -b 4 -c __background__ -bk pytorch
 # Classification sample
 # Batch size 1
 LD_LIBRARY_PATH=./lib ./bin/nvcv_samples_classification -e ./models/resnet50.engine -i ./assets/images/tabby_tiger_cat.jpg -l ./models/imagenet-classes.txt -b 1
